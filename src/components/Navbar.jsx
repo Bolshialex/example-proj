70896a3cdfbccfa3ded5b2cf1502d695b40edc45
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-09-27 22:33:24 -0700

    Add Section 1 page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import reactLogo from "../assets/react.svg";
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <span>ReactFacts</span>
+      </nav>
+    </header>
+  );
+}
